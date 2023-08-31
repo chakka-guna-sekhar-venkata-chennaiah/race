51f345f4c8087d0a2a28077a476cf8e1f4b2870f
chakka-guna-sekhar-venkata-chennaiah <110555361+chakka-guna-sekhar-venkata-chennaiah@users.noreply.github.com>
2023-08-31 16:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,18 @@ if btn and user_image is not None:
 		user_selected_items = list(result[0].keys())
 		if 'dominant_race' in user_selected_items:
 			emotion_label='Race: '+str(result[0]['dominant_race']).title()
+			st.write(emotion_label)
+			cv2.putText(image,'white', (x, y+h+110), cv2.FONT_ITALIC,1 ,(255,255,255), 2)
+	col1,col2=st.columns(2)
+	with col1:
+		st.info('Original Image')
+		st.image(user_image,use_column_width=True)
+	with col2:
+		st.info('Detected Image')
+		st.image(image, use_column_width=True,channels='BGR')
+		
+		st.markdown(f'''<h4 align='center'>Detected Race: {result[0]['dominant_race']}</h1>''',
+
+		unsafe_allow_html=True)
 elif btn and user_image is None:
 	st.warning('Please Check the file')
-			
-			
-
